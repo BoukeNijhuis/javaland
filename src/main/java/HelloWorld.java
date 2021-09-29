42d6f1b081c665f189d51081bccb0f7aa58e7dcc
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2021-09-29 20:51:31 +0200

    finished chapter 2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,10 @@
 public class HelloWorld {
 
     public static void main(String... args) {
+        printHelloWorld();
+    }
 
+    private static void printHelloWorld() {
         System.out.println("Hello world!");
-
     }
 }
