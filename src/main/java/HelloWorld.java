ff4dce269c551bad7fe43d054d503a5af33794bc
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2021-10-06 20:09:00 +0200

    moved some stuff around

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,10 +5,10 @@ public class HelloWorld {
     private String randomString = "random";
 
     public static void main(String... args) {
-        System.out.println(formatMessage("world", HELLO));
+        System.out.println(formatMessage(HELLO, "world"));
     }
 
-    static String formatMessage(String second, String first) {
+    static String formatMessage(String first, String second) {
         return String.format("%s %s!", first, second);
     }
 }
