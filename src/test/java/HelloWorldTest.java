4966134310dcf5857b1e389767fc910b1660012e
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2021-10-06 21:02:09 +0200

    removed a theme

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,12 +1,12 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HelloWorldTest {
 
     @Test
     void main() {
-        String actual = HelloWorld.formatMessage("world", "Hello");
+        String actual = HelloWorld.formatMessage("Hello", "world", "%s %s!");
         assertEquals("Hello world!", actual);
     }
 }
